e3a90e0ec79c02a8fd02ea3ddf18b012b601f1cf
Stephen von Takach <steve@acaprojects.com>
2020-06-02 00:33:51 +1000

    feat: add a helper for cleaning up expired documents

---
+++ config/initializers/cleanup_ttls.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+Thread.new do
+  past = 30.years.ago.to_i
+
+  loop do
+    sleep 10.minutes.to_i
+    begin
+      expired = 10.minutes.ago.to_i
+
+      NoBrainer.run(:db => 'rethinkdb') { |r|
+        r.table('table_config').filter { |sys| sys["indexes"].contains('ttl') }
+      }.map { |table| table['name'] }.each do |table|
+        NoBrainer.run { |r|
+          r.table(table).between(past, expired, index: 'ttl').delete
+        }
+      end
+    rescue error
+      puts "error clearing expired ttl: #{error.message}"
+    end
+  end
+end
