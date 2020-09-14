e4deed7b7c13c80b115317a2d81c0242d761b588
Stephen von Takach <steve@vontaka.ch>
2020-09-14 20:20:09 +1000

    feat(nobrainer): max string length of 64kb

--- config/initializers/nobrainer.rb
+++ config/initializers/nobrainer.rb
@@ -2,8 +2,11 @@
 
 # Here is the place to configure NoBrainer.
 # We are using the default connection settings.
-# NoBrainer.configure do |config|
-#   config.rethinkdb_url = ENV['RDB_URL'] || "rethinkdb://localhost/#{Rails.app.name}_#{Rails.env}"
-# end
+NoBrainer.configure do |config|
+  # config.rethinkdb_url = ENV['RDB_URL'] || "rethinkdb://localhost/#{Rails.app.name}_#{Rails.env}"
+
+  # 64kb
+  config.max_string_length = 65_536
+end
 
 # NoBrainer.sync_indexes
