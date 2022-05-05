513f271f9729675ad81292470ced7df5de893467
Stephen von Takach <steve@place.technology>
2022-05-05 18:34:59 +1000

    chore(auth/coauth_controller): fix style warning

--- app/controllers/auth/coauth_controller.rb
+++ app/controllers/auth/coauth_controller.rb
@@ -23,7 +23,7 @@ module Auth
     def redirect_continue(path)
       # checking for `.attacker.com` OR `//<external_domain>`
       # no internal paths use `//`
-      check_path = path.split('?')[0]
+      check_path = path.split("?")[0]
 
       # need to check if redirect is configured in the DB
       if !check_path.start_with?("/") || check_path.include?("//")
