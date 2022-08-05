d3e467e2bb3ce35de33704cbed5bf095bb889569
Mia Bennett <mia@chillfox.com>
2022-08-05 16:17:10 +0930

    refactor(b2c): cleanup commented out code
    Co-authored-by: Caspian Baska <email@caspian.computer>

--- config/application.rb
+++ config/application.rb
@@ -49,8 +49,6 @@ module EngineApp
       # app.config.middleware.insert_after Rack::Runtime, ::ActionDispatch::Cookies
     end
 
-    # Rails.application.config.middleware.use RewriteCallbackRequest
-    # Rails.application.config.middleware.use RewriteRedirectResponse
     Rails.application.config.middleware.insert_before 0, RewriteCallbackRequest
     Rails.application.config.middleware.insert_before 0, RewriteRedirectResponse
 
